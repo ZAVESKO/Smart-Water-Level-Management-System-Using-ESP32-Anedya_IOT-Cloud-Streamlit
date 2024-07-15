6cc5d720905db0aaadcc75f3559d0440acc1cb55
Harsh Hariyani <164906022+ZAVESKO@users.noreply.github.com>
2024-07-15 16:59:43 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -147,43 +147,6 @@ def drawDashboard():
         # Display the Altair chart using Streamlit
         st.altair_chart(humidity_chart_an, use_container_width=True)
 
-
-    with charts[1]:
-        st.subheader(body="Water Level", anchor=False)
-        if temperatureData.empty:
-            st.write("No Data Available!")
-        else:
-            temperature_chart_an = alt.Chart(data=temperatureData).mark_area(
-                line={'color': '#1fa2ff'},
-                color=alt.Gradient(
-                    gradient='linear',
-                    stops=[alt.GradientStop(color='#1fa2ff', offset=1),
-                        alt.GradientStop(color='rgba(255,255,255,0)', offset=0)],
-                    x1=1,
-                    x2=1,
-                    y1=1,
-                    y2=0,
-                ),
-                interpolate='monotone',
-                cursor='crosshair'
-            ).encode(
-                x=alt.X(
-                    shorthand="Datetime:T",
-                    axis=alt.Axis(format="%Y-%m-%d %H:%M:%S", title="Datetime", tickCount=10, grid=True, tickMinStep=5),
-                ),  # T indicates temporal (time-based) data
-                y=alt.Y(
-                    "aggregate:Q",
-                    # scale=alt.Scale(domain=[0, 100]),
-                    scale=alt.Scale(zero=False, domain=[10, 50]),
-                    axis=alt.Axis(title="Temperature (°C)", grid=True, tickCount=10),
-                ),  # Q indicates quantitative data
-                tooltip=[alt.Tooltip('Datetime:T', format="%Y-%m-%d %H:%M:%S", title="Time",),
-                        alt.Tooltip('aggregate:Q', format="0.2f", title="Value")],
-            ).properties(height=400).interactive()
-
-            st.altair_chart(temperature_chart_an, use_container_width=True)
-
-
 def operateFan():
     if st.session_state.FanState is False:
         anedya_sendCommand("Fan", "ON")
