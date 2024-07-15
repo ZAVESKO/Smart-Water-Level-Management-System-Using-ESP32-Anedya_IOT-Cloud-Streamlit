a12dec868da3c8ae6d4277dc287fb08ba8a5d7b8
Harsh Hariyani <164906022+ZAVESKO@users.noreply.github.com>
2024-07-15 16:56:59 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -117,36 +117,35 @@ def drawDashboard():
     charts = st.columns(2, gap="small")
     with charts[0]:
         st.subheader(body="Water Level ", anchor=False)
-       
-             humidity_chart_an = alt.Chart(data=humidityData).mark_area(
-                line={'color': '#1fa2ff'},
-                color=alt.Gradient(
-                    gradient='linear',
-                    stops=[alt.GradientStop(color='#1fa2ff', offset=1),
-                        alt.GradientStop(color='rgba(255,255,255,0)', offset=0)],
-                    x1=1,
-                    x2=1,
-                    y1=1,
-                    y2=0,
-                ),
-                interpolate='monotone',
-                cursor='crosshair'
-            ).encode(
-                x=alt.X(
-                    shorthand="Datetime:T",
-                    axis=alt.Axis(format="%Y-%m-%d %H:%M:%S", title="Datetime", tickCount=10, grid=True, tickMinStep=5),
-                ),  # T indicates temporal (time-based) data
-                y=alt.Y(
-                    "aggregate:Q",
-                    scale=alt.Scale(domain=[20, 60]),
-                    axis=alt.Axis(title="Humidity (%)", grid=True, tickCount=10),
-                ),  # Q indicates quantitative data
-                tooltip=[alt.Tooltip('Datetime:T', format="%Y-%m-%d %H:%M:%S", title="Time",),
-                        alt.Tooltip('aggregate:Q', format="0.2f", title="Value")],
-            ).properties(height=400).interactive()
+        humidity_chart_an = alt.Chart(data=humidityData).mark_area(
+            line={'color': '#1fa2ff'},
+            color=alt.Gradient(
+                gradient='linear',
+                stops=[alt.GradientStop(color='#1fa2ff', offset=1),
+                    alt.GradientStop(color='rgba(255,255,255,0)', offset=0)],
+                x1=1,
+                x2=1,
+                y1=1,
+                y2=0,
+            ),
+            interpolate='monotone',
+            cursor='crosshair'
+        ).encode(
+            x=alt.X(
+                shorthand="Datetime:T",
+                axis=alt.Axis(format="%Y-%m-%d %H:%M:%S", title="Datetime", tickCount=10, grid=True, tickMinStep=5),
+            ),  # T indicates temporal (time-based) data
+            y=alt.Y(
+                "aggregate:Q",
+                scale=alt.Scale(domain=[20, 60]),
+                axis=alt.Axis(title="Humidity (%)", grid=True, tickCount=10),
+            ),  # Q indicates quantitative data
+            tooltip=[alt.Tooltip('Datetime:T', format="%Y-%m-%d %H:%M:%S", title="Time",),
+                    alt.Tooltip('aggregate:Q', format="0.2f", title="Value")],
+        ).properties(height=400).interactive()
 
-            # Display the Altair chart using Streamlit
-            st.altair_chart(humidity_chart_an, use_container_width=True)
+        # Display the Altair chart using Streamlit
+        st.altair_chart(humidity_chart_an, use_container_width=True)
 
 
     with charts[1]:
